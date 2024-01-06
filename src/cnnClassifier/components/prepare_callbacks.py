1344eba61005f04d7d5ee589f8f73b9293dd9294
Hitarth Shah <hitarth@hitarths-air.lan>
2024-01-05 22:49:54 -0800

    Prepare Callbacks Code Modified

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -8,8 +8,6 @@ from cnnClassifier.entity.config_entity import PrepareCallbacksConfig
 class PrepareCallback:
     def __init__(self, config: PrepareCallbacksConfig):
         self.config = config
-
-
     
     @property
     def _create_tb_callbacks(self):
@@ -20,14 +18,12 @@ class PrepareCallback:
         )
         return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
     
-
     @property
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
-            filepath=self.config.checkpoint_model_filepath,
+            filepath=str(self.config.checkpoint_model_filepath),
             save_best_only=True
         )
-
 
     def get_tb_ckpt_callbacks(self):
         return [
